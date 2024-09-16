68758fd261fdb45d370e1891a1b142eeebbc807a
fgiwilson <iwilson@formativegames.com>
2024-09-15 22:07:14 -0700

    added nav component
    Updated nav component, began basic styling. About to move to feature branches.

---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,13 @@
+<nav class="flex flex-row justify-content w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white ">
+	<div class="flex p-5">
+		<span><img class="flex flex-auto" src="%sveltekit.assets%/RPGA_Logo.jpg" alt=""></span>
+	</div>
+	<div class="flex w-full p-2">
+		<ul class="text-base text-gray-700 pt-4 md:flex md:justify-between md:pt-0 flex flex-row">
+			<li class="md:p-4 py-2 block hover:text-purple-400 px-4"><a href="/">Home</a></li>
+			<li class="md:p-4 py-2 block hover:text-purple-400 px-4"><a href="/worlds/">Your Worlds</a></li>
+			<li class="md:p-4 py-2 block hover:text-purple-400 px-4"><a href="/worlds/campaigns">Your Campaigns</a></li>
+		</ul>
+	</div>
+
+</nav>
--- src/lib/nav.svelte
+++
@@ -1,7 +0,0 @@
-<nav class="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white">
-	<ul class="text-base text-gray-700 pt-4 md:flex md:justify-between md:pt-0">
-		<li class="md:p-4 py-2 block hover:text-purple-400"><a href="/">Home</a></li>
-		<li class="md:p-4 py-2 block hover:text-purple-400"><a href="/worlds/">Your Worlds</a></li>
-		<li class="md:p-4 py-2 block hover:text-purple-400"><a href="/worlds/campaigns">Your Campaigns</a></li>
-	</ul>
-</nav>
