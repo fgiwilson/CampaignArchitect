46d7691b4e1a9befd210cad188de8c529efa7762
Ian Wilson (Formative Games) <iwilson@formativegames.com>
2024-09-16 22:11:53 -0700

    Nav Updates
    Updated colors and padding.

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -2,11 +2,11 @@
 	import Logo from '../../../static/RPGA_Logo.jpg?enhanced'
 </script>
 
-<nav class="flex flex-row justify-content w-full py-4 md:py-0 px-4 text-lg bg-lightBlue">
-	<div class="flex p-5">
-		<enhanced:img class="flex flex-auto w-auto max-h-20" src='../../../static/RPGA_Logo.jpg?w=72' sizes="" alt="">
+<nav class="flex flex-row justify-content w-full pr-4 text-lg bg-logoBeige">
+	<div class="flex pr-2">
+		<enhanced:img class="flex flex-auto w-auto max-h-32" src='../../../static/RPGA_Logo.jpg' alt="">
 	</div>
-	<div class="flex w-full p-2">
+	<div class="flex w-full place-self-center">
 		<ul class="text-base pt-4 md:flex md:justify-between md:pt-0 flex flex-row">
 			<li class="md:p-4 py-2 block text-paynesGray hover:text-cadetGray px-4"><a href="/">Home</a></li>
 			<li class="md:p-4 py-2 block hover:text-purple-400 px-4"><a href="/worlds/">Your Worlds</a></li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ export default {
         paynesGray: '#4F6367',
         beige:'#EEF5DB',
         bittersweet:'#FE5F55',
-        flame:'#DC602E'
+        flame:'#DC602E',
+        lightGray: '#e9e9e9',
+        medGray: '#CCCCCC',
+        gray: '#A6A6A6',
+        darkGray: '#878787',
+        logoBeige: '#edce98'
     },
     extend: {},
   },
